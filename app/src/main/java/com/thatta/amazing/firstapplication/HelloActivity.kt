5e276ce6fce8eb787283bd19b1e41632e709f42f
ThattaPy <sergioalvarez3000@gmail.com>
2022-06-27 14:13:47 +0200

    crated binding in greeting

--- app/src/main/java/com/thatta/amazing/firstapplication/HelloActivity.kt
+++ app/src/main/java/com/thatta/amazing/firstapplication/HelloActivity.kt
@@ -2,10 +2,14 @@ package com.thatta.amazing.firstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thatta.amazing.firstapplication.databinding.ActivityHelloBinding
 
 class HelloActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHelloBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hello)
+        binding = ActivityHelloBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
