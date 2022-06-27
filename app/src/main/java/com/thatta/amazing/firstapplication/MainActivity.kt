1749236b3834b024bf6c1c6b548cffacc153c687
ThattaPy <sergioalvarez3000@gmail.com>
2022-06-27 14:11:45 +0200

    code mainActivity created

--- app/src/main/java/com/thatta/amazing/firstapplication/MainActivity.kt
+++ app/src/main/java/com/thatta/amazing/firstapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.thatta.amazing.firstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tilName = findViewById<TextInputEditText>(R.id.til_name)
+        val btnGreeting = findViewById<Button>(R.id.btn_greeting)
+
+        btnGreeting.setOnClickListener {
+            val name = tilName.text
+            val intent = Intent(this, HelloActivity::class.java)
+            intent.putExtra("name", name ?: "desconocido")
+            startActivity(intent)
+        }
+
     }
 }
